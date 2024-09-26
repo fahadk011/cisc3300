ac3a22b6b5633a8f0bd1223420b477835def4aff
fahad <fahadkhan@nestmk12.net>
2024-09-25 20:38:45 -0400

    in class 4

---
+++ inclass4/in-class-4.js
@@ -0,0 +1,11 @@
+console.log("in-class-4.js loaded!");
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        console.log(`${arr[i]} is even!`);
+    } else {
+        console.log(`${arr[i]} is odd!`);
+    }
+}
